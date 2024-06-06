709a40a7765ebb27fe23eaa9f55990cef65467da
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2024-06-06 18:58:04 -0300

    feat: add find by attendee id method to check in repository

--- src/main/java/macielvini/com/pass_in/repositories/CheckInRepository.java
+++ src/main/java/macielvini/com/pass_in/repositories/CheckInRepository.java
@@ -3,5 +3,8 @@ package macielvini.com.pass_in.repositories;
 import macielvini.com.pass_in.domain.checkIn.CheckIn;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CheckInRepository extends JpaRepository<CheckIn, Integer> {
+    Optional<CheckIn> findByAttendeeId(String attendeeId);
 }
