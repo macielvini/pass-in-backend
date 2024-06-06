ab9411287a116a5bb53dbd61d1b7a6a15e961597
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2024-06-06 17:44:17 -0300

    fix: wrong column name

--- src/main/java/macielvini/com/pass_in/services/EventService.java
+++ src/main/java/macielvini/com/pass_in/services/EventService.java
@@ -3,11 +3,14 @@ package macielvini.com.pass_in.services;
 import lombok.RequiredArgsConstructor;
 import macielvini.com.pass_in.domain.attendee.Attendee;
 import macielvini.com.pass_in.domain.event.Event;
+import macielvini.com.pass_in.dto.event.EventIdDto;
+import macielvini.com.pass_in.dto.event.EventRequestDto;
 import macielvini.com.pass_in.dto.event.EventResponseDto;
 import macielvini.com.pass_in.repositories.AttendeeRepository;
 import macielvini.com.pass_in.repositories.EventRepository;
 import org.springframework.stereotype.Service;
 
+import java.text.Normalizer;
 import java.util.List;
 
 @Service
@@ -20,5 +23,24 @@ public class EventService {
         Event event = this.eventRepository.findById(id).orElseThrow(() -> new RuntimeException("Event not found with ID: " + id));
         List<Attendee> attendeeList = this.attendeeRepository.findByEventId(id);
         return new EventResponseDto(event, attendeeList.size());
+    }
+
+    public EventIdDto createEvent(EventRequestDto eventDto) {
+        Event event = new Event();
+        event.setTitle(eventDto.title());
+        event.setDetail(eventDto.detail());
+        event.setMaximumAttendees(eventDto.maximumAttendees());
+        event.setSlug(this.createSlug(eventDto.title()));
+        this.eventRepository.save(event);
+        return new EventIdDto(event.getId());
+    }
+
+    private String createSlug(String text) {
+        String normalized = Normalizer.normalize(text, Normalizer.Form.NFD);
+        return normalized
+                .replaceAll("[^\\p{InCOMBINING_DIACRITICAL_MARKS}]", "")
+                .replaceAll("[^\\w\\s]", "")
+                .replaceAll("[\\s+]", "-")
+                .toLowerCase();
     }
 }
--- src/main/resources/db/migration/V1__create-table-events.sql
+++ src/main/resources/db/migration/V1__create-table-events.sql
@@ -1,7 +1,8 @@
-CREATE TABLE events (
-    id VARCHAR(255) NOT NULL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    details VARCHAR(255) NOT NULL,
-    slug VARCHAR(255) NOT NULL,
-    maximum_attendees INTEGER NOT NULL
+CREATE TABLE events
+(
+    id                VARCHAR(255) NOT NULL PRIMARY KEY,
+    title             VARCHAR(255) NOT NULL,
+    detail            VARCHAR(255) NOT NULL,
+    slug              VARCHAR(255) NOT NULL,
+    maximum_attendees INTEGER      NOT NULL
 );
