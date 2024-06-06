9d27d37785ee59fa0972e5c95d94abd3308c9c70
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2024-06-06 11:53:34 -0300

    chore: project init

---
+++ src/test/java/macielvini/com/pass_in/PassInApplicationTests.java
@@ -0,0 +1,13 @@
+package macielvini.com.pass_in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
