a2c308e7002b5107822b3982bbc13b85eb464d74
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2024-06-06 16:52:54 -0300

    feat: adds findByEventId method

--- src/main/java/macielvini/com/pass_in/repositories/AttendeeRepository.java
+++ src/main/java/macielvini/com/pass_in/repositories/AttendeeRepository.java
@@ -3,5 +3,8 @@ package macielvini.com.pass_in.repositories;
 import macielvini.com.pass_in.domain.attendee.Attendee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AttendeeRepository extends JpaRepository<Attendee, String> {
+    List<Attendee> findByEventId(String eventId);
 }
