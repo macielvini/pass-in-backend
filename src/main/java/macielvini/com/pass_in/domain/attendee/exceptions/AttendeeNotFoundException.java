f1cd347bdb2d7edd1c4a3f68eaaecc5cabf92bbf
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2024-06-08 14:18:23 -0300

    feat: add attendee not found exception

---
+++ src/main/java/macielvini/com/pass_in/domain/attendee/exceptions/AttendeeNotFoundException.java
@@ -0,0 +1,7 @@
+package macielvini.com.pass_in.domain.attendee.exceptions;
+
+public class AttendeeNotFoundException extends RuntimeException {
+    public AttendeeNotFoundException(String message) {
+        super(message);
+    }
+}
